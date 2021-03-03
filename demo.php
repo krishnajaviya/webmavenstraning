99ad3e953a1721d4743fc7dc63d0b1352594768a
krishna <Krishna.webmavens@gmail.com>
2021-03-03 18:09:19 +0530

    practise bootstrap

--- demo.php
+++ demo.php
@@ -4,27 +4,100 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Hello, world!</title>
+     <title>Hello, world!</title>
   </head>
   <body>
-    <label for="size">Size</label>
-    <label for="binding" align="right">Binding</label>
-    <label for="color" align="center">Color</label>  
-  <br>
-  <select name="size" id="size" class="form-select" aria-label="Default select example">
-    <option value="6*9">6*9</option>
-    <option value="7*10">7*10</option>
-    <option value="8*10">8*10</option>
-  </select> 
-  <select name="binding" id="binding" class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-    <option value="spiral">spiral</option>
-    <option value="perfect">perfect</option>
-  </select>
-  <select name="color" id="color"class="form-select"">
-    <option value="Black n White Interior / Color Cover">Black n White Interior / Color Cover</option>
-    <option value="Full Color Interior / Color Cover">Full Color Interior / Color Cover</option>
-    <option value="Cream Color Interior / Color Cover">Cream Color Interior / Color Cover</option>
-  </select>
+     <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">CompanyName</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportcontent">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportcontent">
+          <ul class ="navbar-nav ml-auto">
+            <li class="nev-item">
+            <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nev-item">
+            <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nev-item dropdown">
+            <a class="nev-link dropdown-toggle" id="navbardropdown" data-toggle="dropdown" href="#">Product</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">product1</a>
+              <a class="dropdown-item" href="#">product1</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">product1</a>
+            </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div class="jumbotron">
+        <h1 class="display-4">simple. elegant.awesome</h1>
+        <p class="lead"> represents text which is marked or highlighted for reference or notation purposes.represents side-comments and small print, like copyright and legal text.</p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button">learn more</a>
+        </p>        
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-body text-center">
+            <h5 class="card-title">card-title</h5>
+            <p class="card-text">some quick text to build up on the card title </p>
+            <a href="#" class="card-link">another link</a>
+          </div> 
+       </div>
+     </div>
+      <div class="col">
+         <div class="card">
+          <div class="card-body text-center">
+            <h5 class="card-title">card-title</h5>
+            <p class="card-text">some quick text to build up on the card title </p>
+            <a href="#" class="card-link">another link</a>
+          </div> 
+        </div>
+      </div>
+      <div class="col">
+         <div class="card">
+          <div class="card-body text-center">
+            <h5 class="card-title">card-title</h5>
+            <p class="card-text">some quick text to build up on the card title </p>
+            <a href="#" class="card-link">another link</a>
+          </div> 
+      </div>
+    </div>
+    <div class="col">
+    <div class="card">
+          <div class="card-body text-center">
+            <h5 class="card-title">card-title</h5>
+            <p class="card-text">some quick text to build up on the card title </p>
+            <a href="#" class="card-link">another link</a>
+          </div> 
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 col-md-8">
+        <h3>Importantant heading</h3>
+        <p class="lead">represents a span of inline text which should be rendered in a way that indicates that it has a non-textual annotation.</p>
+      </div>
+      <div class="col-sm-12 col-md-4">
+        <h3 class="mb-4">secondary menu</h3>
+        <ul class="nav flex-column nav-pills">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">active</a>
+          </li>
+           <li class="nav-item">
+            <a class="nav-link" href="#">link</a>
+          </li>
+           <li class="nav-item">
+            <a class="nav-link disabled" href="#">disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
