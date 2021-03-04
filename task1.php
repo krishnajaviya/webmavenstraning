71bbc8e1d0d1251eff28f22883aaffb2d71e3fc4
krishna <Krishna.webmavens@gmail.com>
2021-03-04 11:58:26 +0530

    formating complete

---
+++ task1.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title></title>
+</head>	
+<body>
+	<div class="row">
+		<div class="col">
+			<label for="size">size</label>
+			<div class="dropdownmenu">
+				<select class="form-control" arial-label="Default-select example">
+        			<option value="6*10">6*10</option>
+          			<option value="6*10">7*10</option>
+          			<option value="6*10">8*10</option>
+          		</select>
+       		</div>
+     	</div>
+        <div class="col">
+        	<label for="binding">binding</label>
+        	<div class="dropdownmenu">
+        		<select class="form-control" arial-label="Default-select example">
+        			<option value="spiral">spiral</option>
+        			<option value="perfect">perfect</option>
+          		</select>
+          	</div> 
+        </div>
+        <div class="col">
+        	<label for="color" align="center">Color</label>
+        	<div class="dropdownmenu">
+        		<select class="form-control" arial-label="Default-select example">
+        			<option value="Black n White Interior / Color Cover">Black n White Interior / Color Cover</option>
+            		<option value="Full Color Interior / Color Cover">Full Color Interior / Color Cover</option>
+            		<option value="Cream Color Interior / Color Cover">Cream Color Interior / Color Cover</option>
+          		</select>
+          	</div> 
+        </div>
+        <div class="col">
+    		<label for="lamination">Lamination</label>
+    		<div class="dropdownmenu">
+    			<select class="form-control" arial-label="Default-select example">
+    				<option value="Glossy">Glossy</option>
+            		<option value="Matt">Matt</option>
+          		</select>
+          	</div> 
+        </div>
+    </div>
+    <div class="row">	
+    <div class="col-md-3">
+    	<label for ="pages">pages</label>
+    	<div class="text">
+    		<input type="text" class="form-control"name="pages"/>
+        </div>
+    </div>
+    <div class="col-md-3">
+    	<label for="qty">quantity</label>
+    	<div class="input-group mb-1">
+    		<input type="text" name="qty" class="form-control" aria-describedby="basic-addon3"/>
+        </div>
+    </div>
+	</div>
+	<div class="row">
+		<button type="button" class="btn btn-primary">submit</button>
+	</div>
+
+	
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
