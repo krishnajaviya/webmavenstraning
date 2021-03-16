4dd48898c23ffb04d191fc491bc61f0032c84c4f
krishna <Krishna.webmavens@gmail.com>
2021-03-16 18:00:51 +0530

    update working

--- product.php
+++ product.php
@@ -1,24 +1,46 @@
 <?php 
-
+include "functions.php";
+$id=get('id');
 // error_reporting(0);
 // ini_set('display_errors', 1);
 // ini_set('display_startup_errors', 1);
 // error_reporting(E_ALL);
+include "dbconn.php";
 
-include "dbConn.php";
+// $name=get('name');
+// $slug=get('slug');
+// $sku=get('sku');
+// $moq=get('moq');
+// $categories=get('categories');
+// $search_keywords=get('search_keywords');
+// $price=get('price');
+// $discount_type=get('discount_type');
+// $discount_value=get('discount_value');
+
+// $insert= array();
+// $insert['name']=$name;
+// $insert['slug']=$slug;
+// $insert['sku']=$sku;
+// $insert['moq']=$moq;
+// $insert['categories']=$categories;
+// $insert['search_keywords']=$search_keywords;
+// $insert['price']=$price;
+// $insert['discount_type']=$discount_type;
+// $insert['discount_value']=$discount_value;
+
 $name = $slug = $sku = $moq = $categories = $search_keywords = $price = $discount_type = $discount_value ='';
 $error = array('name' =>'', 'slug' =>'', 'sku' => '','moq'=>'','categories'=>'','search_keywords'=>'','price'=>'','discount_type'=>'','discount_value'=>'');
 		if (isset($_POST['submit'])) {
-			if (empty($_POST['name'])) {
+			if (empty($_POST['name'])){
  				$error['name']="name can be required" ;
  			}else{
- 			$name = $_POST['name'];
+ 			    $name = $_POST['name'];
 	 			if(!preg_match('/^[A-Za-z-]+$/', $name)){
 	 			$error['name'] =  "name can be only letter" ;
 	 			}
  			}
 		 	if (empty($_POST['slug'])){
-		 		$error['slug'] =  "slug can be only letter" ;
+		 		$error['slug'] =  "slug can be required" ;
 		 	}else{
 			 	$slug = $_POST['slug'];
 	 			if(!preg_match('/^[A-Za-z-]+$/', $slug)){
@@ -26,7 +48,7 @@ $error = array('name' =>'', 'slug' =>'', 'sku' => '','moq'=>'','categories'=>'',
 	 			}
 		 	}
 		 	if(empty($_POST['sku'])){
-		 		$error['sku'] =  "sku can be only letter" ;
+		 		$error['sku'] =  "sku can be required" ;
 		 	}else{
 			 	$sku = $_POST['sku'];
 	 			if(!preg_match('/^[A-Za-z-]+$/', $sku)){
@@ -42,7 +64,7 @@ $error = array('name' =>'', 'slug' =>'', 'sku' => '','moq'=>'','categories'=>'',
  				}
  			}
 		    if(empty($_POST['categories'])){
-		    	$error['categories'] =  "categories can be only letter" ;
+		    	$error['categories'] =  "categories can be required" ;
 		 	}else{
 			 	$categories = $_POST['categories'];
 	 			if(!preg_match('/^[A-Za-z-]+$/', $categories)){
@@ -51,7 +73,7 @@ $error = array('name' =>'', 'slug' =>'', 'sku' => '','moq'=>'','categories'=>'',
 
 		    }
 		    if(empty($_POST['search_keywords'])){
-		    	$error['search_keywords'] =  "search_keywords can be only letter" ;
+		    	$error['search_keywords'] =  "search_keywords can be required" ;
 		 	}else{
 			 	$search_keywords = $_POST['search_keywords'];
 	 			if(!preg_match('/^[A-Za-z-]+$/', $search_keywords)){
@@ -67,7 +89,7 @@ $error = array('name' =>'', 'slug' =>'', 'sku' => '','moq'=>'','categories'=>'',
 		 		}
 		 	}
 		 	 if(empty($_POST['discount_type'])){
-		    	$error['discount_type'] =  "discount_type can be only letter" ;
+		    	$error['discount_type'] =  "discount_type can be required" ;
 		 	}else{
 			 	$discount_type = $_POST['discount_type'];
 	 			if(!preg_match('/^[A-Za-z-]+$/', $discount_type)){
@@ -89,12 +111,13 @@ $error = array('name' =>'', 'slug' =>'', 'sku' => '','moq'=>'','categories'=>'',
 		 		$slug = mysqli_real_escape_string($db, $_POST['slug']);
 		 		$sku = mysqli_real_escape_string($db, $_POST['sku']);
 		 		$moq = mysqli_real_escape_string($db, $_POST['moq']);
-		 		$categories = mysqli_real_escape_string($db, $_POST['categories']);
+		 		$categories = mysqli_real_escape_string($db,$_POST['categories']);
 		 		$search_keywords = mysqli_real_escape_string($db, $_POST['search_keywords']);
 		 		$price = mysqli_real_escape_string($db, $_POST['price']);
 		 		$discount_type = mysqli_real_escape_string($db, $_POST['discount_type']);
 		 		$discount_value = mysqli_real_escape_string($db, $_POST['discount_value']);
-				    
+
+				// $query= insert('product',$insert,$db);
 			    $sql = mysqli_query($db,"INSERT INTO product(name, slug, sku, moq, categories, search_keywords, price, discount_type, discount_value) VALUES('$name', '$slug','$sku', '$moq', '$categories', '$search_keywords', '$price','$discount_type', '$discount_value')");
 			     if(!$sql)
 			    {
@@ -110,6 +133,9 @@ $error = array('name' =>'', 'slug' =>'', 'sku' => '','moq'=>'','categories'=>'',
 		}
 
 ?>
+<?php 
+//if($id !=get('id')){?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -118,46 +144,123 @@ $error = array('name' =>'', 'slug' =>'', 'sku' => '','moq'=>'','categories'=>'',
 <body>
 	<section class="container grey-text">
 	<form method="POST" action="">
-		
-	
-		Name: <input type="text" name="name" value="<?php echo htmlspecialchars($name) ?>" >
-  		<div class="red-text"><?php echo $error['name'];?></div>
+		<input type="hidden" name="id">
+		Name: <input type="text" name="name" value="<?php echo htmlspecialchars($name)?>" required>
+  		<span id="name" style="color: red"><?php echo $error['name'];?></span>
   		<br>
-		
-		Slug:<input type="text" name="slug" value="<?php echo htmlspecialchars($slug) ?>">
-		<div class="red-text"><?php echo $error['slug'];?></div>
 		<br>
-
-		Sku :<input type="text" name="sku" value="<?php echo htmlspecialchars($sku) ?>">
-		<div class="red-text"><?php echo $error['sku'];?></div>
+		Slug:<input type="text" name="slug" value="<?php echo htmlspecialchars($slug) ?>" required>
+		<span id="slug" style="color: red"><?php echo $error['slug'];?></span>
 		<br>
-		
-		moq :<input type="number" name="moq" value="<?php echo htmlspecialchars($moq) ?>">
-		<div class="red-text"><?php echo $error['moq'];?></div>
 		<br>
-		
-		categories :<input type="text" name="categories" value="<?php echo htmlspecialchars($categories) ?>">
-		<div class="red-text"><?php echo $error['categories'];?></div>
+		Sku :<input type="text" name="sku" value="<?php echo htmlspecialchars($sku) ?>" required>
+		<span id="sku" style="color: red"><?php echo $error['sku'];?></span>
 		<br>
-		
-		search_keywords:<input type="text" name="search_keywords" value="<?php echo htmlspecialchars($search_keywords) ?>">
-		<div class="red-text"><?php echo $error['search_keywords'];?></div>
 		<br>
-		
-		price :<input type="number" name="price" value="<?php echo htmlspecialchars($price) ?>">
-		<div class="red-text"><?php echo $error['price'];?></div>
+		moq :<input type="number" name="moq" value="<?php echo htmlspecialchars($moq) ?>" required>
+		<span id="moq" style="color: red"><?php echo $error['moq'];?></span>
 		<br>
-		
-		discount_type :<input type="number" name="discount_type" value="<?php echo htmlspecialchars($discount_type) ?>">
-		<div class="red-text"><?php echo $error['discount_type'];?></div>
 		<br>
-		
-		discount_value :<input type="text" name="discount_value" value="<?php echo htmlspecialchars($discount_value) ?>">
-		<div class="red-text"><?php echo $error['discount_value'];?></div>
+		categories :<input type="text" name="categories" value="<?php echo htmlspecialchars($categories) ?>" required>
+		<span id="categories" style="color: red"><?php echo $error['categories'];?></span>
+		<br>
+		<br>
+		search_keywords:<input type="text" name="search_keywords" value="<?php echo htmlspecialchars($search_keywords) ?>" required>
+		<span id="search_keywords" style="color: red"><?php echo $error['search_keywords'];?></span>
+		<br>
+		<br>
+		price :<input type="number" name="price" value="<?php echo htmlspecialchars($price) ?>" required>
+		<span id="price" style="color:red"><?php echo $error['price'];?></span>
+		<br>
+		<br>
+		discount_type :<input type="text" name="discount_type" value="<?php echo htmlspecialchars($discount_type) ?>" required>
+		<span id="discount_type" style="color: red"><?php echo $error['discount_type'];?></span>
+		<br>
+		<br>
+		discount_value :<input type="number" name="discount_value" value="<?php echo htmlspecialchars($discount_value) ?>" required>
+		<span id="discount_value" style="color: red"><?php echo $error['discount_value'];?></span>
+		<br>
 		<br>
 		<input type="submit" name="submit" value="submit">
 		</form>
 	</section>
 </body>
 </html>
-		
+<?php 
+if($id=get('id')){?>
+
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<section class="container grey-text">
+		<form method="POST"  >
+			<!-- <input type="hidden" name="id"> -->
+			Name123: <input type="text" name="name" value="" Required>
+	  	    
+	  		<br>
+			<br>
+			Slug:<input type="text" name="slug" value="" required>
+			<!-- <span id="slug" style="color: red"><?php echo $error['slug'];?></span> -->
+			<br>
+			<br>
+			Sku :<input type="text" name="sku" value="" required>
+			<!-- <span id="sku" style="color: red"><?php echo $error['sku'];?></span> -->
+			<br>
+			<br>
+			moq :<input type="number" name="moq" value="" required>
+			<!-- <span id="moq" style="color: red"><?php echo $error['moq'];?></span> -->
+			<br>
+			<br>
+			categories :<input type="text" name="categories" value="" required>
+			<!-- <span id="categories" style="color: red"><?php echo $error['categories'];?></span> -->
+			<br>
+			<br>
+			search_keywords:<input type="text" name="search_keywords" value="" required>
+			<!-- <span id="search_keywords" style="color: red"><?php echo $error['search_keywords'];?></span> -->
+			<br>
+			<br>
+			price :<input type="number" name="price" value="" required>
+			<!-- <span id="price" style="color:red"><?php echo $error['price'];?></span> -->
+			<br>
+			<br>
+			discount_type :<input type="text" name="discount_type" value="" required>
+			<!-- <span id="discount_type" style="color: red"><?php echo $error['discount_type'];?></span> -->
+			<br>
+			<br>
+			discount_value :<input type="number" name="discount_value" value="" required>
+			<!-- <span id="discount_value" style="color: red"><?php echo $error['discount_value'];?></span> -->
+			<br>
+			<br>
+			<input type="submit" name="submit" value="submit">
+			</form>
+		</section>
+	</body>
+	</html>
+<?php }
+$select=mysqli_query($db,"SELECT * FROM product WHERE id='$id'");
+
+$raw =mysqli_fetch_array($select);
+
+if(isset($_POST['submit'])){
+	$name=$_POST['name'];
+	$slug=$_POST['slug'];
+	$sku=$_POST['sku'];
+	$moq=$_POST['moq'];
+	$categories=$_POST['categories'];
+	$search_keywords=$_POST['search_keywords'];
+	$price=$_POST['price']; 
+	$discount_type=$_POST['discount_type'];
+	$discount_value=$_POST['discount_value'];
+
+	$edit=mysqli_query($db,"UPDATE product SET name='$name', slug='$slug', sku = '$sku' ,moq='$moq', categories='$categories',search_keywords='$search_keywords', price='$price', discount_type='$discount_type',discount_value='$discount_value' WHERE id= '$id'");
+	if($edit){
+		header("location:product_form.php");
+		exit;
+	}else{
+		mysqli_error();
+	}
+}
+?>
